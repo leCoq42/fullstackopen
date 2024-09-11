562cc29e615b3aef5634ae87d0bf20c4e09269af
Martin <mhaan@student.codam.nl>
2024-09-11 17:19:05 +0200

    part2.d done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,23 +1,25 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import phonebookService from "./services/phonebook";
 
-const Person = ({ name, num }) => {
+const Person = ({ person, deletePerson }) => {
   return (
     <li>
-      {name} {num}
+      {person.name} {person.number}{" "}
+      <button onClick={() => deletePerson(person)}>delete</button>
     </li>
   );
 };
 
-const Persons = ({ persons, filter }) => {
+const Persons = ({ persons, filter, deletePerson }) => {
   let filter_lowered = filter.toLowerCase();
   let filteredPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(filter_lowered),
   );
   const personElem = filteredPersons.map((person) => {
-    return <Person key={person.name} name={person.name} num={person.number} />;
+    return (
+      <Person key={person.name} person={person} deletePerson={deletePerson} />
+    );
   });
-
   return <ul>{personElem}</ul>;
 };
 
@@ -46,12 +48,10 @@ const PersonForm = ({
 
 const Filter = ({ newFilter, handleFilterChange }) => {
   return (
-    <form>
-      <div>
-        filter shown with:{" "}
-        <input name="filter" value={newFilter} onChange={handleFilterChange} />
-      </div>
-    </form>
+    <div>
+      filter shown with:{" "}
+      <input name="filter" value={newFilter} onChange={handleFilterChange} />
+    </div>
   );
 };
 
@@ -59,15 +59,13 @@ const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNum, setNewNum] = useState("");
-  const [newFilter, setNewFilter] = useState("");
+  const [filterStr, setNewFilter] = useState("");
 
-  const hook = () => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
+  useEffect(() => {
+    phonebookService.getAll().then((response) => {
+      setPersons(response);
     });
-  };
-
-  useEffect(hook, []);
+  }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -77,14 +75,45 @@ const App = () => {
       number: newNum,
     };
 
-    if (persons.some((person) => person.name === newName)) {
-      const msg = `${newName} is already added to phonebook`;
-      alert(msg);
-      return false;
+    const duplicate = persons.find((person) => person.name === newName);
+
+    if (duplicate) {
+      const msg = `${duplicate.name} is already added to phonebook, replace the old number with a new one?`;
+      if (window.confirm(msg) === true) {
+        phonebookService
+          .update(duplicate.id, personObject)
+          .then((ret) => {
+            setPersons(persons.map((p) => (p.id !== duplicate.id ? p : ret)));
+          })
+          .catch((error) => {
+            console.log("Error updating person: ", error.message);
+            alert("Error updating person.");
+          });
+        return false;
+      }
     }
-    setPersons(persons.concat(personObject));
-    setNewName("");
-    setNewNum("");
+
+    phonebookService.create(personObject).then((response) => {
+      setPersons(persons.concat(response));
+      setNewName("");
+      setNewNum("");
+    });
+  };
+
+  const deletePerson = (person) => {
+    const msg = `Really delete ${person.name}?`;
+
+    if (window.confirm(msg) === true) {
+      phonebookService
+        .remove(person.id)
+        .then(() => {
+          setPersons(persons.filter((p) => p.id !== person.id));
+        })
+        .catch((error) => {
+          console.log("Error deleting person: ", error.message);
+          alert("Error deleting person.");
+        });
+    }
   };
 
   const handleNameChange = (event) => setNewName(event.target.value);
@@ -94,7 +123,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
+      <Filter newFilter={filterStr} handleFilterChange={handleFilterChange} />
       <h3>Add a new</h3>
       <PersonForm
         addPerson={(obj) => addPerson(obj)}
@@ -104,7 +133,11 @@ const App = () => {
         handleNumChange={(obj) => handleNumChange(obj)}
       />
       <h3>Numbers</h3>
-      <Persons persons={persons} filter={newFilter} />
+      <Persons
+        persons={persons}
+        filter={filterStr}
+        deletePerson={deletePerson}
+      />
     </div>
   );
 };
